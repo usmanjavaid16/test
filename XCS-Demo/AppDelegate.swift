a707359041be863c28b7fb7bdee6cf4cb8a4debf
Usman Javaid <usmanjavaid@tintash.com>
2017-10-12 15:02:08 +0500

    A commit on master.

--- XCS-Demo/AppDelegate.swift
+++ XCS-Demo/AppDelegate.swift
@@ -16,8 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let ret = true
-        return ret
+        return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
